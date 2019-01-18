8a4270272a3b63515020e7b118c35964962fce1c
Phil Marcoccia <36013742+frostie@users.noreply.github.com>
2019-01-18 09:43:08 -0500

    Update hello-world.cpp

--- hello-world.cpp
+++ hello-world.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 int main()
 {
-  std::cout <<"Hello World! My name is Phil, and this is my repository! It will be used primarily for game dev projects and other snippets of code that I would like to share!" << std::endl;
+  std::cout <<"Hello World! My name is Phil, and this is my repository! It will be used primarily for game dev projects and other programming projects that I feel act as a showcase of my programming abilities!" << std::endl;
   return 0;
 }
